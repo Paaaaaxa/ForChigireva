527232d2122e3ecaa37ef02ca8f0937abed25d7d
Paaaaaxa <100375205+Paaaaaxa@users.noreply.github.com>
2022-03-08 19:59:26 +0300

    Добавление комментариев

--- GaussCode.py
+++ GaussCode.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+
+#вводим матрицы А и В
+
+
 N = int(input())
 # MatrixA = np.zeros((N,N), dtype=float)
 # MatrixB = np.zeros((N,1), dtype=float)
@@ -19,29 +23,30 @@ MatrixBBasic = MatrixB.copy()
 # начало прямого хода метода Гаусса
 for i in range(N):
     iMaxNumber = np.where(MatrixA[i:N, i] == max(MatrixA[i:N, i]))  # находим строку с наибольшим элементом в столбце(как оказалось, это массив)
-    m = MatrixA[i:N, i]  # Это большой прикол, но похоже, что это именно ссылка на объект
-    MasCopyA = np.copy(MatrixA[iMaxNumber[0] + i])  # это глубокая копия и два независимых массива
+    # Это большой прикол, но похоже, что это именно ссылка на объект
+    MasCopyA = np.copy(MatrixA[iMaxNumber[0] + i]) #копируем нцжную строку для двух массивов А и В
+    # это глубокая копия и два независимых массива
     MasCopyB = np.copy(MatrixB[iMaxNumber[0] + i])
     MatrixA[iMaxNumber[0] + i] = MatrixA[i]
     MatrixB[iMaxNumber[0] + i] = MatrixB[i]
     MatrixA[i] = MasCopyA
     MatrixB[i] = MasCopyB
-    MatrixA[i], MatrixB[i][0] = MatrixA[i] / MatrixA[i][i], MatrixB[i][0] / MatrixA[i][i]
+    MatrixA[i], MatrixB[i][0] = MatrixA[i] / MatrixA[i][i], MatrixB[i][0] / MatrixA[i][i] # нормируемся на элемент, стоящий в главной диаганали
 
+    #   фонмируем нули в нижней части матрицы путемвычитания
     for j in range(i + 1, N):
         MatrixB[j][0] = MatrixB[j][0] - MatrixB[i][0] * MatrixA[j][i]
-        MatrixA[j][i:N] = MatrixA[j][i:N] - MatrixA[i][i:N] * MatrixA[j][
-            i]  # вычитаем строки срезы можно использовать и так: m = MatrixA[2,0:2]
+        MatrixA[j][i:N] = MatrixA[j][i:N] - MatrixA[i][i:N] * MatrixA[j][i]  # вычитаем строки срезы можно использовать и так: m = MatrixA[2,0:2]
 
     print(MatrixA)  # печать на каждом шаге
     print(MatrixB)
 # обратный ход
-# print(MatrixB)
+
 for i in reversed(range(N)):
+    # находим значение каждой переменной, начиная с последней, и переносим полученные значения в правую часть
     current = MatrixB[i][0] * MatrixA[0:i, i]  # необходимо использовать так как надо иметь транспонированную
-    o = MatrixB[0:i, 0]
-    MatrixB[0:i, 0] = MatrixB[0:i, 0] - current.transpose()
-    MatrixA[0:i, i] = 0.0
+    MatrixB[0:i, 0] = MatrixB[0:i, 0] - current.transpose() # производим вычитание из правой части
+    MatrixA[0:i, i] = 0.0 #  формируем нули в верхней части матрицы А
     print(MatrixA)  # печать на каждом шаге
     print(MatrixB)
 
@@ -71,7 +76,7 @@ BRelativeError = BAbsoluteError/Norma(MatrixBBasic)
 
 #Получим оценку числа обусловленности
 print("Число обусловленности не менее данной величины")
-print()
+print(BRelativeError/XRelativeError)
 
 
 
