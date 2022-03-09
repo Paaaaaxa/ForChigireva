66753ddc230792ae5efb99353be5010bae115ea6
Paaaaaxa <100375205+Paaaaaxa@users.noreply.github.com>
2022-03-09 20:32:08 +0300

    Реализация красивого вывода

--- GaussCode.py
+++ GaussCode.py
@@ -3,14 +3,21 @@ import numpy as np
 
 #вводим матрицы А и В
 
-
+print("Введите размерность матрицы")
 N = int(input())
 # MatrixA = np.zeros((N,N), dtype=float)
 # MatrixB = np.zeros((N,1), dtype=float)
+# XVectorExactly = np.zeros((N,1), dtype=float)
+# print("Введите матрицу А")
 # for i in range(N):
 #     MatrixA[i] = [float(y) for y in input().split()]
+# print("Введите матрицу B")
 # for i in range(N):
 #     MatrixB[i] = float(input())
+# print("Введите матрицу точных решений")
+# for i in range(N):
+#     XVectorExactly[i] = float(input())
+
 
 MatrixA = np.array([[8, 2, 5], [3, 4, 7], [9, 7, 4]], float)
 MatrixB = np.array([[8], [7], [5]], float)
@@ -22,6 +29,7 @@ MatrixBBasic = MatrixB.copy()
 
 # начало прямого хода метода Гаусса
 for i in range(N):
+
     iMaxNumber = np.where(MatrixA[i:N, i] == max(MatrixA[i:N, i]))  # находим строку с наибольшим элементом в столбце(как оказалось, это массив)
     # Это большой прикол, но похоже, что это именно ссылка на объект
     MasCopyA = np.copy(MatrixA[iMaxNumber[0] + i]) #копируем нцжную строку для двух массивов А и В
@@ -37,18 +45,25 @@ for i in range(N):
     for j in range(i + 1, N):
         MatrixB[j][0] = MatrixB[j][0] - MatrixB[i][0] * MatrixA[j][i]
         MatrixA[j][i:N] = MatrixA[j][i:N] - MatrixA[i][i:N] * MatrixA[j][i]  # вычитаем строки срезы можно использовать и так: m = MatrixA[2,0:2]
-
+    print("Прямой ход шаг номер: ", i+1)
+    print("Матрица А")
     print(MatrixA)  # печать на каждом шаге
+    print("Матрица В")
     print(MatrixB)
 # обратный ход
+print("Прямой ход завершен")
 
 for i in reversed(range(N)):
     # находим значение каждой переменной, начиная с последней, и переносим полученные значения в правую часть
     current = MatrixB[i][0] * MatrixA[0:i, i]  # необходимо использовать так как надо иметь транспонированную
     MatrixB[0:i, 0] = MatrixB[0:i, 0] - current.transpose() # производим вычитание из правой части
     MatrixA[0:i, i] = 0.0 #  формируем нули в верхней части матрицы А
+    print("Обратный ход шаг номер: ", N - i)
+    print("Матрица А")
     print(MatrixA)  # печать на каждом шаге
+    print("Матрица В")
     print(MatrixB)
+print("Обратный ход завершен, решение системы найдено.")
 
 
 def Norma(Matrix: np):
